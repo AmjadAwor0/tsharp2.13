49d719ed9e7dca055aa3888e651a435c640c9402
TSharp Team <81733820+AmjadAwor0@users.noreply.github.com>
2023-11-02 07:28:55 +0000

    Delete Test.py

--- Test.py
+++
@@ -1,5 +0,0 @@
-list1 = ["Amjad","Zen","Belal",6,23,12,58,21]
-for i in range(0,len(list1)):
-	for j in range(i+1,len(list1)):
-		if list1[i] >= list1[j]:
-			list1[i], list1[j] = list1[j], list1[i]
